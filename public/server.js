3406dd3dbb04d15c9ccb235c1fe93b268e31d8ea
donmaleek <bamboloshari@gmail.com>
2025-05-27 13:18:55 +0300

    Initial commit2

--- public/server.js
+++ public/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '.')));
 app.use(express.static(path.join(__dirname,)));
 
 // MySQL Connection Pool
